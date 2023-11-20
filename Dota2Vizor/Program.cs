28a7dd7ed739dffa9c40aa075a4950ecd83dd364
weakbtw <artemtnt42@gmail.com>
2023-11-20 21:49:26 +0200

    first

---
+++ Dota2Vizor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Chat
+{
+    private List<string> messages = new List<string>();
+
+    public void PostMessage(string user, string message)
+    {
+        messages.Add(user + ": " + message);
+    }
+
+    public void DisplayChat()
+    {
+        foreach (var message in messages)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Chat chat = new Chat();
+
+        while (true)
+        {
+            Console.Write("Enter your name: ");
+            string user = Console.ReadLine();
+
+            Console.Write("Enter your message: ");
+            string message = Console.ReadLine();
+
+            chat.PostMessage(user, message);
+
+            Console.WriteLine("\nChat:");
+            chat.DisplayChat();
+            Console.WriteLine();
+        }
+    }
+}
